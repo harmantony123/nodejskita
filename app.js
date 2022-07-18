c9ffa676a2aa0dc44cfaff3717997d3ea8dfe5a4
Harman <harmanelins@gmail.com>
2022-07-19 05:54:55 +0700

    menambah package baru

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use("/", (req, res, next) => {
   res.sendFile(path.join(__dirname, "view", "index.html"));
 });
